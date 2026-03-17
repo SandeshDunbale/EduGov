f317253d162b0f2fb5a7c1d2a8c0d099c7035752
rohantade8 <rohantade8@gmail.com>
2026-03-17 23:19:05 +0530

    commit for researchprojectrepo

---
+++ EduGov/src/main/java/com/project/edugov/repository/ResearchProjectRepository.java
@@ -0,0 +1,6 @@
+package com.project.edugov.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.edugov.model.ResearchProject;
+
+public interface ResearchProjectRepository extends JpaRepository<ResearchProject, Long> {
+}
