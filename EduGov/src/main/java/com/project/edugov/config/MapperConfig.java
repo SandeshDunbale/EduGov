61309b1606e0fa4c53c6093a36b90b3b77e5be20
vaishnavi <vaishnavisirsat96@gmail.com>
2026-03-26 10:17:26 +0530

    Added mapper in config package

--- EduGov/src/main/java/com/project/edugov/config/MapperConfig.java
+++ EduGov/src/main/java/com/project/edugov/config/MapperConfig.java
@@ -8,26 +8,29 @@ import com.project.edugov.dto.FacultyMinimalDTO;
 import com.project.edugov.dto.GrantResponseDTO;
 import com.project.edugov.model.Faculty;
 import com.project.edugov.model.Grant;
+import com.project.edugov.model.GrantApplication;
 
 @Configuration
 public class MapperConfig {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
 
-        // 1. Mapping for Faculty -> FacultyMinimalDTO
-        modelMapper.typeMap(Faculty.class, FacultyMinimalDTO.class).addMappings(mapper -> {
-            mapper.map(src -> src.getUser().getName(), FacultyMinimalDTO::setFacultyName);
-        });
+		modelMapper.typeMap(Faculty.class, FacultyMinimalDTO.class).addMappings(mapper -> {
+			mapper.map(src -> src.getUser().getName(), FacultyMinimalDTO::setFacultyName);
+		});
 
-        // 2. Mapping for Grant -> GrantResponseDTO (Fetching only Role)
-        modelMapper.typeMap(Grant.class, GrantResponseDTO.class).addMappings(mapper -> {
-            // Converts Enum Role to String automatically
-            mapper.map(src -> src.getApprovedBy().getRole(), GrantResponseDTO::setApprovedByRole);
-            mapper.map(src -> src.getProject().getTitle(), GrantResponseDTO::setProjectTitle);
-        });
+		modelMapper.typeMap(Grant.class, GrantResponseDTO.class).addMappings(mapper -> {
 
-        return modelMapper;
-    }
+			mapper.map(src -> src.getApprovedBy().getRole(), GrantResponseDTO::setApprovedByRole);
+			mapper.map(src -> src.getProject().getTitle(), GrantResponseDTO::setProjectTitle);
+		});
+
+		modelMapper.typeMap(GrantApplication.class, GrantResponseDTO.class).addMappings(mapper -> {
+
+			mapper.map(src -> src.getProject().getTitle(), GrantResponseDTO::setProjectTitle);
+		});
+		return modelMapper;
+	}
 }
