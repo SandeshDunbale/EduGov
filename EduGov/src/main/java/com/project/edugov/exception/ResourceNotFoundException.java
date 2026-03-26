41a6d710cd3f3f444f39ad69ce1b772e692b3ce6
vaishnavi <vaishnavisirsat96@gmail.com>
2026-03-26 10:13:18 +0530

    Updates in Exception

--- EduGov/src/main/java/com/project/edugov/exception/ResourceNotFoundException.java
+++ EduGov/src/main/java/com/project/edugov/exception/ResourceNotFoundException.java
@@ -1,14 +1,12 @@
 package com.project.edugov.exception;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// This ensures that even if we don't catch it, it defaults to a 404 status
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
 }
